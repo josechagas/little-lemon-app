53ca61026ce45955f7870aa016bc4c060013495b
José Lucas Souza das Chagas <chagasjoselucas@gmail.com>
2023-02-08 14:59:04 -0300

    Initial Commit

---
+++ Little Lemon/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  Little Lemon
+//
+//  Created by José Lucas Souza das Chagas on 08/02/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
